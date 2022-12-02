7ec192a4b2547999b2d2e2aedce98c4f7ef33149
Holly <h.person.notrobot@gmail.com>
2022-12-02 11:44:47 +0000

    day 1.1

--- src/1.rs
+++ src/1.rs
@@ -13,23 +13,27 @@ fn main() {
 9000
 
 10000".split("\n\n");
-    let mut max:i32 = 0;
-    let mut max_elf_inv = "";
-    let mut max_elf_index = 0;
-    let mut pos = 0;
+    let mut max = vec![];
+    let mut temp:i32 = 0;
+    //let mut max_elf_inv = "";
+    //let mut max_elf_index = 0;
+    //let mut pos = 0;
     for p in info{
-        pos += 1;
-        let mut temp:i32 = 0;
+        //pos += 1;
+        temp = 0;
         for p2 in p.split("\n"){
             temp += p2.parse::<i32>().unwrap();
         }
-        if temp > max {
-            max = temp;
-            max_elf_inv = p;
-            max_elf_index = pos;
-        }
+        max.push(temp)
     }
-    println!("Max: {} kcal", max);
-    println!("Elf inv: \n{}", max_elf_inv);
-    println!("Elf number {}", max_elf_index)
+    max.sort();
+    max.reverse();
+
+    println!("Max: {} kcal", max[0]+max[1]+max[2]);
+    println!("Max: {} kcal", max[0]);
+    println!("Max: {} kcal", max[1]);
+    println!("Max: {} kcal", max[2]);
+
+    //println!("Elf inv: \n{}", max_elf_inv);
+    //println!("Elf number {}", max_elf_index);
 }

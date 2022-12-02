5ccd8203fe15d668d077df810ec881ed18a742d0
Holly <h.person.notrobot@gmail.com>
2022-12-02 20:43:18 +0000

    day 2.1

--- 2.rs
+++ 2.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 fn main() {
 
-    let states = HashMap::from([
+    let states = HashMap::from([    // Part 1
         ("A X", 4),
         ("A Y", 8),
         ("A Z", 3),
@@ -15,6 +15,18 @@ fn main() {
         ("C Y", 2),
         ("C Z", 6),
     ]);
+    /*let states = HashMap::from([   // Part 2
+        ("A X", 3),
+        ("A Y", 4),
+        ("A Z", 8),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 2),
+        ("C Y", 6),
+        ("C Z", 7),
+    ]);*/
+
 
     let input = fs::read_to_string("./input.txt")
         .expect("Plz be something in this file");

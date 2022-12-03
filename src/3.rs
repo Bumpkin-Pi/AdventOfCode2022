f43390eceefb57764d76555c1c49d0f9a727dfd2
Holly <h.person.notrobot@gmail.com>
2022-12-03 10:49:21 +0000

    day 3

---
+++ src/3.rs
@@ -0,0 +1,28 @@
+use std::fs;
+// use std::collections::HashMap;
+
+
+
+fn main() {
+
+    let input = fs::read_to_string("./input_d2.txt")
+        .expect("Plz be something in this file");
+    let data = input.split("\n").collect::<Vec<&str>>();
+
+    let mut total_value:u32 = 0;
+
+    for line in data{
+        let first_half = &line[..(line.chars().count()/2)];
+        let second_half = &line[(line.chars().count()/2)..];
+
+
+        for char in first_half.chars(){
+            if second_half.contains(char){
+                let value:u32 = if 97 <= (char as u32) && (char as u32) <= 122 { char as u32 - 96 }else{ char as u32 - 38 };
+                total_value += value;
+                break;
+            }
+        }
+    }
+    println!("{}", total_value)
+}
